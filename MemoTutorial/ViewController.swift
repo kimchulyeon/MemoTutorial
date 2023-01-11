d348bcb3809a2931962a5fc1963fdc50784164c6
chulyeon <guinness987@gmail.com>
2023-01-11 16:00:27 +0900

    Initial Commit

---
+++ MemoTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoTutorial
+//
+//  Created by chulyeon kim on 2023/01/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
